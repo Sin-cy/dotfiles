0c7467de57d753f922f3e6f567ab088296812d1f
Lord Sinquaad <Setthananpanichnok@gmail.com>
2024-06-05 09:25:42 +0700

    added tailwind.lua plugin file

--- nvim/.config/nvim/lua/sethy/plugins/formatting.lua
+++ nvim/.config/nvim/lua/sethy/plugins/formatting.lua
@@ -6,7 +6,7 @@ return {
 
         conform.setup({
             formatters_by_ft = {
-                javascript = { "stylua" },
+                javascript = { "prettier" },
                 typescript = { "prettier" },
                 javascriptreact = { "prettier" },
                 typescriptreact = { "prettier" },
