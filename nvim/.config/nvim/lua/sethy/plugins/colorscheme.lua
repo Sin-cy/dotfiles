0cd28bd37e9070d39a25ec2743c0dcefb54d9324
Lord Sinquaad <Setthananpanichnok@gmail.com>
2024-09-09 17:26:45 +0700

    updated colorscheme.lua

--- nvim/.config/nvim/lua/sethy/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/sethy/plugins/colorscheme.lua
@@ -4,133 +4,125 @@ local function ColorMyPencils(color)
 
 	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
 	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
-
 end
 
 return {
-    -- NOTE: Tokyo night
-    {
-        "folke/tokyonight.nvim",
-        name = 'folkeTokyonight',
-        priority = 1000,
-        config = function()
-            local transparent = false
+	-- NOTE: Tokyo night
+	{
+		"folke/tokyonight.nvim",
+		name = "folkeTokyonight",
+		priority = 1000,
+		config = function()
+			local transparent = false
 
-            local bg = "#011628"
-            local bg_dark = "#011423"
-            local bg_highlight = "#143652"
-            local bg_search = "#0A64AC"
-            local bg_visual = "#275378"
-            local fg = "#CBE0F0"
-            local fg_dark = "#B4D0E9"
-            local fg_gutter = "#627E97"
-            local border = "#547998"
+			local bg = "#011628"
+			local bg_dark = "#011423"
+			local bg_highlight = "#143652"
+			local bg_search = "#0A64AC"
+			local bg_visual = "#275378"
+			local fg = "#CBE0F0"
+			local fg_dark = "#B4D0E9"
+			local fg_gutter = "#627E97"
+			local border = "#547998"
 
-            require("tokyonight").setup({
-                style = "night",
-                transparent = transparent,
+			require("tokyonight").setup({
+				style = "night",
+				transparent = transparent,
 
-                styles = {
-                    keywords = { italic = false },
-                    sidebars = transparent and 'transparent' or 'dark',
-                    floats = transparent and 'transparent' or 'dark',
-                },
-                on_colors = function(colors)
-                    colors.bg = bg
-                    colors.bg_dark = transparent and colors.none or bg_dark
-                    colors.bg_float = transparent and colors.none or bg_dark
-                    colors.bg_highlight = bg_highlight
-                    colors.bg_popup = bg_dark
-                    colors.bg_search = bg_search
-                    colors.bg_sidebar = transparent and colors.none or bg_dark
-                    colors.bg_statusline = transparent and colors.none or bg_dark
-                    colors.bg_visual = bg_visual
-                    colors.border = border
-                    colors.fg = fg
-                    colors.fg_dark = fg_dark
-                    colors.fg_float = fg
-                    colors.fg_gutter = fg_gutter
-                    colors.fg_sidebar = fg_dark
-                end,
-            })
-            -- vim.cmd("colorscheme tokyonight")
+				styles = {
+					keywords = { italic = false },
+					sidebars = transparent and "transparent" or "dark",
+					floats = transparent and "transparent" or "dark",
+				},
+				on_colors = function(colors)
+					colors.bg = bg
+					colors.bg_dark = transparent and colors.none or bg_dark
+					colors.bg_float = transparent and colors.none or bg_dark
+					colors.bg_highlight = bg_highlight
+					colors.bg_popup = bg_dark
+					colors.bg_search = bg_search
+					colors.bg_sidebar = transparent and colors.none or bg_dark
+					colors.bg_statusline = transparent and colors.none or bg_dark
+					colors.bg_visual = bg_visual
+					colors.border = border
+					colors.fg = fg
+					colors.fg_dark = fg_dark
+					colors.fg_float = fg
+					colors.fg_gutter = fg_gutter
+					colors.fg_sidebar = fg_dark
+				end,
+			})
+			-- vim.cmd("colorscheme tokyonight")
 
-            -- ColorMyPencils("tokyonight")
+			-- ColorMyPencils("tokyonight")
+		end,
+	},
 
+	-- NOTE: Rose pine
+	{
+		"rose-pine/neovim",
+		name = "rose-pine",
+		priority = 1000,
+		config = function()
+			require("rose-pine").setup({
 
-       end
+				variant = "main", -- auto, main, moon, or dawn
+				dark_variant = "main", -- main, moon, or dawn
+				dim_inactive_windows = false,
+				disable_background = true,
+				disable_nc_background = false,
+				disable_float_background = false,
+				extend_background_behind_borders = false,
 
-    },
+				enable = {
+					terminal = true,
+					legacy_highlights = true, -- Improve compatibility for previous versions of Neovim
+					migrations = true, -- Handle deprecated options automatically
+				},
 
-    -- NOTE: Rose pine
-    {
-        "rose-pine/neovim",
-        name = "rose-pine",
-        config = function ()
+				styles = {
+					bold = true,
+					italic = false,
+					transparency = true,
+				},
 
-            local transparent = false
+				groups = {
+					border = "muted",
+					link = "iris",
+					panel = "surface",
 
-            require("rose-pine").setup({
-                variant = "auto", -- auto, main, moon, or dawn
-                dark_variant = "main", -- main, moon, or dawn
-                transparent = transparent,
-                dim_inactive_windows = false,
-                disable_background = true,
-                disable_nc_background = false,
-                disable_float_background = false,
-                extend_background_behind_borders = true,
+					error = "love",
+					hint = "iris",
+					info = "foam",
+					note = "pine",
+					todo = "rose",
+					warn = "gold",
 
-                enable = {
-                    terminal = true,
-                    legacy_highlights = true, -- Improve compatibility for previous versions of Neovim
-                    migrations = true, -- Handle deprecated options automatically
-                },
+					git_add = "foam",
+					git_change = "rose",
+					git_delete = "love",
+					git_dirty = "rose",
+					git_ignore = "muted",
+					git_merge = "iris",
+					git_rename = "pine",
+					git_stage = "iris",
+					git_text = "rose",
+					git_untracked = "subtle",
 
-                styles = {
-                    bold = true,
-                    italic = false,
-                    transparency = true,
-                },
+					h1 = "iris",
+					h2 = "foam",
+					h3 = "rose",
+					h4 = "gold",
+					h5 = "pine",
+					h6 = "foam",
+				},
+			})
 
-                groups = {
-                    border = "muted",
-                    link = "iris",
-                    panel = "surface",
+			-- NOTE: set this on the color you want to be persistent
+			-- when quit and reopening nvim
+			-- vim.cmd("colorscheme rose-pine")
 
-                    error = "love",
-                    hint = "iris",
-                    info = "foam",
-                    note = "pine",
-                    todo = "rose",
-                    warn = "gold",
-
-                    git_add = "foam",
-                    git_change = "rose",
-                    git_delete = "love",
-                    git_dirty = "rose",
-                    git_ignore = "muted",
-                    git_merge = "iris",
-                    git_rename = "pine",
-                    git_stage = "iris",
-                    git_text = "rose",
-                    git_untracked = "subtle",
-
-                    h1 = "iris",
-                    h2 = "foam",
-                    h3 = "rose",
-                    h4 = "gold",
-                    h5 = "pine",
-                    h6 = "foam",
-                },
-            })
-
-            -- NOTE: set this on the color you want to be persistent
-            -- when quit and reopening nvim
-            -- vim.cmd("colorscheme rose-pine")
-
-            ColorMyPencils("rose-pine")
-
-
-        end
-    },
+			ColorMyPencils("rose-pine")
+		end,
+	},
 }
