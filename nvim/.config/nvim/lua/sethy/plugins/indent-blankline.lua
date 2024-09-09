5c5e328d8e661b01e7c0f5ebe8e088c2b14ff8eb
Lord Sinquaad <Setthananpanichnok@gmail.com>
2024-09-09 20:36:26 +0700

    remove indent underlines in indent-blanklines config

--- nvim/.config/nvim/lua/sethy/plugins/indent-blankline.lua
+++ nvim/.config/nvim/lua/sethy/plugins/indent-blankline.lua
@@ -1,8 +1,12 @@
 return {
-  "lukas-reineke/indent-blankline.nvim",
-  event = { "BufReadPre", "BufNewFile" },
-  main = "ibl",
-  opts = {
-    indent = { char = "┊" },
-  },
+	"lukas-reineke/indent-blankline.nvim",
+	event = { "BufReadPre", "BufNewFile" },
+	main = "ibl",
+	opts = {
+		indent = { char = "┊" },
+		scope = {
+			show_start = false,
+			show_end = false,
+		},
+	},
 }
