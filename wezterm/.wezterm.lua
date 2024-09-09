787f70d9d1707cc8d8ba63941f5a8bc8b1384e5b
Lord Sinquaad <Setthananpanichnok@gmail.com>
2024-09-10 02:22:19 +0700

    wezterm changes

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -1,44 +1,45 @@
 local wezterm = require("wezterm")
 local config = wezterm.config_builder()
 
-config.font = wezterm.font("JetBrainsMonoNL Nerd Font")
 config.font_size = 16
+config.font = wezterm.font("JetBrainsMonoNL Nerd Font")
 config.automatically_reload_config = true
 config.enable_tab_bar = false
 config.window_decorations = "RESIZE"
 config.adjust_window_size_when_changing_font_size = false
-config.harfbuzz_features = { 'calt=0' }
--- config.window_background_opacity = 0.75
--- config.macos_window_background_blur = 10
+config.harfbuzz_features = { "calt=0" }
+config.window_background_opacity = 0.75
+config.macos_window_background_blur = 50
+
 config.window_padding = {
-  left = 20,
-  right = 15,
-  top = 30,
-  bottom = 20,
+	left = 10,
+	right = 5,
+	top = 30,
+	bottom = 10,
 }
 
 -- Key bindings for zoom and delete word
 config.keys = {
-    {
-        key = "+",
-        mods = "CMD",
-        action = wezterm.action.IncreaseFontSize,
-    },
-    {
-        key = "-",
-        mods = "CMD",
-        action = wezterm.action.DecreaseFontSize,
-    },
-    {
-        key="LeftArrow",
-        mods="OPT",
-        action=wezterm.action{SendString="\x1bb"}
-    },
-    {
-        key="RightArrow",
-        mods="OPT",
-        action=wezterm.action{SendString="\x1bf"}
-    },
+	{
+		key = "+",
+		mods = "CMD",
+		action = wezterm.action.IncreaseFontSize,
+	},
+	{
+		key = "-",
+		mods = "CMD",
+		action = wezterm.action.DecreaseFontSize,
+	},
+	{
+		key = "LeftArrow",
+		mods = "OPT",
+		action = wezterm.action({ SendString = "\x1bb" }),
+	},
+	{
+		key = "RightArrow",
+		mods = "OPT",
+		action = wezterm.action({ SendString = "\x1bf" }),
+	},
 }
 
 -- Configs
@@ -46,8 +47,8 @@ config.keys = {
 config.color_scheme = "rose-pine"
 
 config.colors = {
-    background = "#1a1a1c",
+	background = "#1a1a1c",
+	-- background = "#191919",
 }
 
 return config
-
