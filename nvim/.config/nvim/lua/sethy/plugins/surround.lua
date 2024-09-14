d9b465bd8c228871cdc850036257c1e93f8bfcec
Lord Sinquaad <136789131+Sin-cy@users.noreply.github.com>
2024-09-15 01:59:25 +0700

    quality of life changes

--- nvim/.config/nvim/lua/sethy/plugins/surround.lua
+++ nvim/.config/nvim/lua/sethy/plugins/surround.lua
@@ -1,6 +1,7 @@
 return {
-  "kylechui/nvim-surround",
-  event = { "BufReadPre", "BufNewFile" },
-  version = "*", -- Use for stability; omit to use `main` branch for the latest features
-  config = true,
+	--might just turn this off
+	"kylechui/nvim-surround",
+	event = { "BufReadPre", "BufNewFile" },
+	version = "*", -- Use for stability; omit to use `main` branch for the latest features
+	config = true,
 }
