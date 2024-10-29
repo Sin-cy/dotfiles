bd1ba8d7defc2739316dadc95eed8f45493bccdc
Lord Sinquaad <136789131+Sin-cy@users.noreply.github.com>
2024-10-29 19:10:21 +0700

    added gruvbox colorscheme

--- nvim/.config/nvim/lua/sethy/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/sethy/plugins/colorscheme.lua
@@ -1,5 +1,5 @@
 local function ColorMyPencils(color)
-	color = color or "rose-pine" or "tokyonight"
+	color = color or "rose-pine" or "tokyonight" or "gruvbox"
 	vim.cmd.colorscheme(color)
 
 	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
@@ -7,7 +7,6 @@ local function ColorMyPencils(color)
 end
 
 return {
-	-- NOTE: Tokyo night borrowed custom theme from Josean
 	{
 		"folke/tokyonight.nvim",
 		name = "folkeTokyonight",
@@ -128,4 +127,33 @@ return {
 			ColorMyPencils("rose-pine")
 		end,
 	},
+    { "ellisonleao/gruvbox.nvim",
+        priority = 1000 ,
+        config = function()
+            require("gruvbox").setup({
+                terminal_colors = true, -- add neovim terminal colors
+                undercurl = true,
+                underline = true,
+                bold = true,
+                italic = {
+                    strings = false,
+                    emphasis = false,
+                    comments = false,
+                    operators = false,
+                    folds = false,
+                },
+                strikethrough = true,
+                invert_selection = false,
+                invert_signs = false,
+                invert_tabline = false,
+                invert_intend_guides = false,
+                inverse = true, -- invert background for search, diffs, statuslines and errors
+                contrast = "", -- can be "hard", "soft" or empty string
+                palette_overrides = {},
+                overrides = {},
+                dim_inactive = false,
+                transparent_mode = true,
+            })
+        end
+    }
 }
