2f73f0409aab9f468952ecae087f632be6500762
Lord Sinquaad <136789131+Sin-cy@users.noreply.github.com>
2024-11-09 20:31:08 +0700

    quality of life updates

--- nvim/.config/nvim/lua/sethy/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/sethy/plugins/colorscheme.lua
@@ -1,10 +1,3 @@
-local function ColorMyPencils(color)
-	color = color or "rose-pine" or "tokyonight" or "gruvbox"
-	vim.cmd.colorscheme(color)
-
-	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
-end
 
 return {
 	{
