7d5ec6b1312c2bb28e81bef293a2c33b5e5ce874
Lord Sinquaad <136789131+Sin-cy@users.noreply.github.com>
2024-11-10 05:12:45 +0700

    Huh ?_?

--- nvim/.config/nvim/lua/sethy/plugins/emmet.lua
+++ nvim/.config/nvim/lua/sethy/plugins/emmet.lua
@@ -1,4 +1,6 @@
 return {
+    -- here only for wrapping html tags
+    -- emmet_ls is already installed in mason
         "olrtg/nvim-emmet",
         config = function()
             vim.keymap.set({ "n", "v" }, '<leader>xe', require('nvim-emmet').wrap_with_abbreviation)
