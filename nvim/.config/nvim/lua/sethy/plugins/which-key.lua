0a6d8af63f566596713f97ca0fcb8d58f3e1d32f
Lord Sinquaad <136789131+Sin-cy@users.noreply.github.com>
2024-11-11 05:15:31 +0700

    I wanna say quality of life updates but yea

--- nvim/.config/nvim/lua/sethy/plugins/which-key.lua
+++ nvim/.config/nvim/lua/sethy/plugins/which-key.lua
@@ -1,12 +1,13 @@
 return {
-	-- I got annoyed so I just stopped using it for a bit
-	-- "folke/which-key.nvim",
-	-- event = "VeryLazy",
-	-- init = function()
-	--     vim.o.timeout = true
-	--     vim.o.timeoutlen = 500
-	-- end,
-	-- opts = {
- --        -- empty for default settings
-	-- }
+	--I got annoyed so I just stopped using it for a bit
+	"folke/which-key.nvim",
+    enabled = false,
+	event = "VeryLazy",
+	init = function()
+	    vim.o.timeout = true
+	    vim.o.timeoutlen = 500
+	end,
+	opts = {
+        -- empty for default settings
+	}
 }
