c0002ca4da5a3433cabe810b7eccb87675dcd321
Lord Sinquaad <136789131+Sin-cy@users.noreply.github.com>
2024-11-12 18:16:16 +0700

    blahblah

--- nvim/.config/nvim/lua/sethy/plugins/ui.lua
+++ nvim/.config/nvim/lua/sethy/plugins/ui.lua
@@ -1,32 +1,39 @@
 return {
-    -- Adding a filename to the Top Right
-    {
-        "b0o/incline.nvim",
-        dependencies = {},
-        event = "BufReadPre",
-        priority = 1200,
-        config = function()
-            local helpers = require("incline.helpers")
-            require("incline").setup({
-                window = {
-                    padding = 0,
-                    margin = { horizontal = 0 },
+	-- Adding a filename to the Top Right
+	{
+		"b0o/incline.nvim",
+		dependencies = {},
+		event = "BufReadPre",
+		priority = 1200,
+		config = function()
+			local devicons = require("nvim-web-devicons")
+			require("incline").setup({
+                highlight = {
+                    groups = {
+                        InclineNormal = { guibg = "#1a1a1c", guifg = "#a9b1d6" },
+                        InclineNormalNC = { guibg = "none", guifg = "#a9b1d6" },
+                    },
                 },
-                render = function(props)
-                    local filename = vim.fn.fnamemodify(vim.api.nvim_buf_get_name(props.buf), ":t")
-                    local ft_icon, ft_color = require("nvim-web-devicons").get_icon_color(filename)
-                    local modified = vim.bo[props.buf].modified
-                    local buffer = {
-                        ft_icon and { " ", ft_icon, " ", guibg = ft_color, guifg = helpers.contrast_color(ft_color) }
-                        or "",
-                        " ",
-                        { filename, gui = modified and "bold,italic" or "bold" },
-                        " ",
-                        guibg = "#363944",
+				window = {
+					padding = 0,
+					margin = { horizontal = 1 },
+				},
+				render = function(props)
+					local filename = vim.fn.fnamemodify(vim.api.nvim_buf_get_name(props.buf), ":t")
+					if vim.bo[props.buf].modified then
+						filename = "[*]" .. filename
+					end
+
+					local icon, color = devicons.get_icon_color(filename)
+
+					return {
+                        { " " },
+                        { icon, { " " } , guifg = color },
+                        { filename },
+                        { " " }
                     }
-                    return buffer
-                end,
-            })
-        end,
-    },
+				end,
+			})
+		end,
+	},
 }
