169484941d549182cf7bbaa2cefdbc3a33b7625f
Lord Sinquaad <136789131+Sin-cy@users.noreply.github.com>
2024-11-12 18:47:04 +0700

    qolu

--- nvim/.config/nvim/lua/sethy/plugins/ui.lua
+++ nvim/.config/nvim/lua/sethy/plugins/ui.lua
@@ -1,39 +1,34 @@
 return {
-	-- Adding a filename to the Top Right
-	{
-		"b0o/incline.nvim",
-		dependencies = {},
-		event = "BufReadPre",
-		priority = 1200,
-		config = function()
-			local devicons = require("nvim-web-devicons")
-			require("incline").setup({
-                highlight = {
-                    groups = {
-                        InclineNormal = { guibg = "#1a1a1c", guifg = "#a9b1d6" },
-                        InclineNormalNC = { guibg = "none", guifg = "#a9b1d6" },
-                    },
+    -- Adding a filename to the Top Right
+    {
+        "b0o/incline.nvim",
+        dependencies = {},
+        event = "BufReadPre",
+        priority = 1200,
+        config = function()
+            local devicons = require("nvim-web-devicons")
+            require("incline").setup({
+                window = {
+                    padding = 0,
+                    margin = { horizontal = 0 },
                 },
-				window = {
-					padding = 0,
-					margin = { horizontal = 1 },
-				},
-				render = function(props)
-					local filename = vim.fn.fnamemodify(vim.api.nvim_buf_get_name(props.buf), ":t")
-					if vim.bo[props.buf].modified then
-						filename = "[*]" .. filename
-					end
+                render = function(props)
+                    local filename = vim.fn.fnamemodify(vim.api.nvim_buf_get_name(props.buf), ":t")
+                    local filetype = vim.bo[props.buf].filetype
+                    local icon, color = devicons.get_icon_color_by_filetype(filetype)
 
-					local icon, color = devicons.get_icon_color(filename)
-
-					return {
-                        { " " },
-                        { icon, { " " } , guifg = color },
-                        { filename },
-                        { " " }
+                    local modified = vim.bo[props.buf].modified
+                    local buffer = {
+                        icon and { " ", icon, " ", guifg = color }
+                        or "",
+                        " ",
+                        { filename, gui = modified and "bold" },
+                        " ",
+                        guibg = "#000",
                     }
-				end,
-			})
-		end,
-	},
+                    return buffer
+                end,
+            })
+        end,
+    },
 }
