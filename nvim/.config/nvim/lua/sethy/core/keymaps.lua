2329ffdc3915e70c48e5fa95df18db1030e76925
Lord Sinquaad <136789131+Sin-cy@users.noreply.github.com>
2024-11-22 02:47:58 +0700

    updates to keymaps and surround plugins

--- nvim/.config/nvim/lua/sethy/core/keymaps.lua
+++ nvim/.config/nvim/lua/sethy/core/keymaps.lua
@@ -3,26 +3,23 @@ vim.g.maplocalleader = " "
 
 local opts = { noremap = true, silent = true }
 
-vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
-vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv", { desc = "moves lines down in visual selection" })
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv", { desc = "moves lines up in visual selection" })
 
 vim.keymap.set("n", "J", "mzJ`z")
-vim.keymap.set("n", "<C-d>", "<C-d>zz")
-vim.keymap.set("n", "<C-u>", "<C-u>zz")
+vim.keymap.set("n", "<C-d>", "<C-d>zz", { desc = "move down in buffer with cursor centered" })
+vim.keymap.set("n", "<C-u>", "<C-u>zz", { desc = "move up in buffer with cursor centered" })
 vim.keymap.set("n", "n", "nzzzv")
 vim.keymap.set("n", "N", "Nzzzv")
 
 vim.keymap.set("v", "<", "<gv", opts)
 vim.keymap.set("v", ">", ">gv", opts)
 
--- the how it should be paste
+-- the how it be paste
 vim.keymap.set("x", "<leader>p", [["_dP]])
 
 -- remember yanked
 vim.keymap.set("v", "p", '"_dp', opts)
-
--- Leader yy will not yank to clipboard by asbjornHaland
-vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]])
 
 -- Copies or Yank to system clipboard
 vim.keymap.set("n", "<leader>Y", [["+Y]], opts)
@@ -32,32 +29,39 @@ vim.keymap.set({ "n", "v" }, "<leader>d", [["_d]])
 
 -- ctrl c as escape cuz Im lazy to reach up to the esc key
 vim.keymap.set("i", "<C-c>", "<Esc>")
-vim.keymap.set("n", "<C-c>", ":nohl<CR>", { desc = "Clear search hl",  silent = true })
+vim.keymap.set("n", "<C-c>", ":nohl<CR>", { desc = "Clear search hl", silent = true })
 vim.keymap.set("n", "<leader>f", vim.lsp.buf.format)
 
+-- Unmaps Q in normal mode
 vim.keymap.set("n", "Q", "<nop>")
+
+--Stars new tmux session from in here
 vim.keymap.set("n", "<C-f>", "<cmd>silent !tmux neww tmux-sessionizer<CR>")
 
 -- prevent x delete from registering when next paste
 vim.keymap.set("n", "x", '"_x', opts)
 
-vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
-vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
+-- Replace the word cursor is on globally
+vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]],
+    { desc = "Replace word cursor is on globally" })
+
+-- Executes shell command from in here making file executable
+vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true, desc = "makes file executable" })
 
 -- Hightlight yanking
 vim.api.nvim_create_autocmd("TextYankPost", {
-	desc = "Highlight when yanking (copying) text",
-	group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
-	callback = function()
-		vim.highlight.on_yank()
-	end,
+    desc = "Highlight when yanking (copying) text",
+    group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
+    callback = function()
+        vim.highlight.on_yank()
+    end,
 })
 
 -- tab stuff
-vim.keymap.set("n", "<leader>to", "<cmd>tabnew<CR>") --open new tab
+vim.keymap.set("n", "<leader>to", "<cmd>tabnew<CR>")   --open new tab
 vim.keymap.set("n", "<leader>tx", "<cmd>tabclose<CR>") --close current tab
-vim.keymap.set("n", "<leader>tn", "<cmd>tabn<CR>") --go to next
-vim.keymap.set("n", "<leader>tp", "<cmd>tabp<CR>") --go to pre
+vim.keymap.set("n", "<leader>tn", "<cmd>tabn<CR>")     --go to next
+vim.keymap.set("n", "<leader>tp", "<cmd>tabp<CR>")     --go to pre
 vim.keymap.set("n", "<leader>tf", "<cmd>tabnew %<CR>") --open current tab in new tab
 
 --split management
--- nvim/.config/nvim/lua/sethy/plugins/surround.lua
+++ nvim/.config/nvim/lua/sethy/plugins/surround.lua
@@ -1,9 +1,10 @@
 return {
-	"kylechui/nvim-surround",
-	event = { "BufReadPre", "BufNewFile" },
-	version = "*", -- Use for stability; omit to use `main` branch for the latest features
-	config = true,
-
+    -- Deciding Between Nvim Surround or mini surround
+	-- "kylechui/nvim-surround",
+	-- event = { "BufReadPre", "BufNewFile" },
+	-- version = "*", -- Use for stability; omit to use `main` branch for the latest features
+	-- config = true,
+    --
     -- HACK: The Key Commands -> for help run :h nvim-surround.usage
     --
     --     Old text                    Command         New text
@@ -15,5 +16,46 @@ return {
     --     'change quot*es'            cs'"            "change quotes"
     --     <b>or tag* types</b>        csth1<CR>       <h1>or tag types</h1>
     --     delete(functi*on calls)     dsf             function calls
+    --
+    -- TODO: Mini surrond might just be better
+    "echasnovski/mini.surround",
+    opts = {
+        -- Add custom surroundings to be used on top of builtin ones. For more
+        -- information with examples, see `:h MiniSurround.config`.
+        custom_surroundings = nil,
 
+        -- Duration (in ms) of highlight when calling `MiniSurround.highlight()`
+        highlight_duration = 300,
+
+        -- Module mappings. Use `''` (empty string) to disable one.
+        mappings = {
+            add = 'ad', -- Add surrounding in Normal and Visual modes
+            delete = 'ds', -- Delete surrounding
+            find = 'sf', -- Find surrounding (to the right)
+            find_left = 'sF', -- Find surrounding (to the left)
+            highlight = 'sh', -- Highlight surrounding
+            replace = 'sr', -- Replace surrounding
+            update_n_lines = 'sn', -- Update `n_lines`
+
+            suffix_last = 'l', -- Suffix to search with "prev" method
+            suffix_next = 'n', -- Suffix to search with "next" method
+        },
+
+        -- Number of lines within which surrounding is searched
+        n_lines = 20,
+
+        -- Whether to respect selection type:
+        -- - Place surroundings on separate lines in linewise mode.
+        -- - Place surroundings on each line in blockwise mode.
+        respect_selection_type = false,
+
+        -- How to search for surrounding (first inside current line, then inside
+        -- neighborhood). One of 'cover', 'cover_or_next', 'cover_or_prev',
+        -- 'cover_or_nearest', 'next', 'prev', 'nearest'. For more details,
+        -- see `:h MiniSurround.config`.
+        search_method = 'cover',
+
+        -- Whether to disable showing non-error feedback
+        silent = false,
+    }
 }
