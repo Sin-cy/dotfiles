3a61fdfbfe0549b9b162b0e84893bacd25a5f5a4
Lord Sinquaad <136789131+Sin-cy@users.noreply.github.com>
2024-11-29 12:52:35 +0700

    added scripts
    fzf and zoxide

--- scripts/scripts/fzf_listoldfiles.sh
+++ scripts/scripts/fzf_listoldfiles.sh
@@ -4,23 +4,22 @@
 # set to an alias nlof in .zshrc
 
 list_oldfiles() {
-  # Get the oldfiles list from Neovim
-  local oldfiles=($(nvim -u NONE --headless +'lua io.write(table.concat(vim.v.oldfiles, "\n") .. "\n")' +qa))
-  # Filter invalid paths or files not found
-  local valid_files=()
-  for file in "${oldfiles[@]}"; do
-    if [[ -f "$file" ]]; then
-      valid_files+=("$file")
-    fi
-  done
-  # Use fzf to select from valid files
-  local files=($(printf "%s\n" "${valid_files[@]}" | \
-    grep -v '\[.*' | \
-    fzf --multi \
-      --preview 'bat --style=numbers --color=always --line-range=:500 {} 2>/dev/null || echo "Error previewing file"' \
-      --preview-window=right:50%:wrap \
-      --height=65% \
-      --layout=reverse))
+    # Get the oldfiles list from Neovim
+    local oldfiles=($(nvim -u NONE --headless +'lua io.write(table.concat(vim.v.oldfiles, "\n") .. "\n")' +qa))
+    # Filter invalid paths or files not found
+    local valid_files=()
+    for file in "${oldfiles[@]}"; do
+        if [[ -f "$file" ]]; then
+            valid_files+=("$file")
+        fi
+    done
+    # Use fzf to select from valid files
+    local files=($(printf "%s\n" "${valid_files[@]}" | \
+        grep -v '\[.*' | \
+        fzf --multi \
+        --preview 'bat -n --color=always --line-range=:500 {} 2>/dev/null || echo "Error previewing file"' \
+        --height=70% \
+        --layout=default))
 
   # Open selected files in Neovim
   [[ ${#files[@]} -gt 0 ]] && nvim "${files[@]}"
--- scripts/scripts/zoxide_openfiles_nvim.sh
+++ scripts/scripts/zoxide_openfiles_nvim.sh
@@ -4,13 +4,13 @@
 # alias set as nzo in .zshrc
 search_with_zoxdie() {
     if [ -z "$1" ]; then
-        # No parameter provided, use fzf to select a file interactively
-        file="$(fd --type f -I -H -E .git -E .git-crypt -E .cache -E .backup | fzf-tmux -p -w 80% -h 70% --preview='bat --style=plain --color=always {}')"
+        # use fd with fzf to select & open a file when no arg are provided
+        file="$(fd --type f -I -H -E .git -E .git-crypt -E .cache -E .backup | fzf --height=70% --preview='bat -n --color=always --line-range :500 {}')"
         if [ -n "$file" ]; then
             nvim "$file"
         fi
     else
-        # Handle when an argument is provided
+        # Handle when an arg is provided
         lines=$(zoxide query -l | xargs -I {} fd --type f -I -H -E .git -E .git-crypt -E .cache -E .backup -E .vscode "$1" {} | fzf --no-sort) # Initial filter attempt with fzf
         line_count="$(echo "$lines" | wc -l | xargs)" # Trim any leading spaces
 
@@ -20,7 +20,7 @@ search_with_zoxdie() {
             nvim "$file"
         elif [ -n "$lines" ]; then
             # If multiple files are found, allow further selection using fzf and bat for preview
-            file=$(echo "$lines" | fzf-tmux -p --query="$1" -w 80% -h 70% --preview='bat --style=plain --color=always {}')
+            file=$(echo "$lines" | fzf --query="$1" --height=70% --preview='bat -n --color=always --line-range :500 {}')
             if [ -n "$file" ]; then
                 nvim "$file"
             fi
