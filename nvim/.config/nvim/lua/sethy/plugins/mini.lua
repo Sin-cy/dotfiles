a0d23d49af7fabaa877cc63040bee32bbd246b65
Lord Sinquaad <136789131+Sin-cy@users.noreply.github.com>
2025-02-03 04:08:41 +0700

    removed hi patterns

--- nvim/.config/nvim/lua/sethy/plugins/mini.lua
+++ nvim/.config/nvim/lua/sethy/plugins/mini.lua
@@ -68,28 +68,6 @@ return {
             silent = false,
         },
     },
-    -- Mini hipatterns
-    {
-        "echasnovski/mini.hipatterns",
-        event = "BufReadPre",
-        opts = {},
-
-        config = function()
-            local hipatterns = require("mini.hipatterns")
-            hipatterns.setup({
-                highlighters = {
-                    -- Highlight standalone 'FIXME', 'HACK', 'TODO', 'NOTE'
-                    fixme = { pattern = "%f[%w]()FIXME()%f[%W]", group = "MiniHipatternsFixme" },
-                    hack = { pattern = "%f[%w]()HACK()%f[%W]", group = "MiniHipatternsHack" },
-                    todo = { pattern = "%f[%w]()TODO()%f[%W]", group = "MiniHipatternsTodo" },
-                    note = { pattern = "%f[%w]()NOTE()%f[%W]", group = "MiniHipatternsNote" },
-
-                    -- Highlight hex color strings (`#rrggbb`) using that color
-                    hex_color = hipatterns.gen_highlighter.hex_color(),
-                },
-            })
-        end,
-    },
     -- Get rid of whitespace
     {
         "echasnovski/mini.trailspace",
