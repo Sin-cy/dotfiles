4871cc4a05e380dc9fa2b184b80aca7673c423b8
Lord Sinquaad <136789131+Sin-cy@users.noreply.github.com>
2025-02-03 04:09:20 +0700

    updates...

--- nvim/.config/nvim/lua/sethy/plugins/todo-comments.lua
+++ nvim/.config/nvim/lua/sethy/plugins/todo-comments.lua
@@ -1,20 +1,35 @@
 return {
-    -- Quickly Jump through the todo tags
-    "folke/todo-comments.nvim",
-    event = { "BufReadPre", "BufNewFile" },
-    dependencies = { "nvim-lua/plenary.nvim" },
-    config = function()
-        local todo_comments = require("todo-comments")
+	-- Quickly Jump through the todo tags
+	"folke/todo-comments.nvim",
+	event = { "BufReadPre", "BufNewFile" },
+	dependencies = { "nvim-lua/plenary.nvim"},
+	config = function()
+		local todo_comments = require("todo-comments")
 
-        -- keymaps
-        vim.keymap.set("n", "]t", function()
-            todo_comments.jump_next()
-        end, { desc = "Next todo comment" })
+		todo_comments.setup({
+			keywords = {
+				FIX = {
+					icon = " ", -- icon used for the sign, and in search results
+					color = "error", -- can be a hex color, or a named color (see below)
+					alt = { "FIXME", "BUG", "FIXIT", "ISSUE" }, -- a set of other keywords that all map to this FIX keywords
+					-- signs = false, -- configure signs for some keywords individually
+				},
+				TODO = { icon = " ", color = "info" },
+				HACK = { icon = " ", color = "warning", alt = { "DON SKIP" } },
+				WARN = { icon = " ", color = "warning", alt = { "WARNING", "XXX" } },
+				PERF = { icon = " ", alt = { "OPTIM", "PERFORMANCE", "OPTIMIZE" } },
+				NOTE = { icon = " ", color = "hint", alt = { "INFO", "READ", "COLORS" } },
+				TEST = { icon = "⏲ ", color = "test", alt = { "TESTING", "PASSED", "FAILED" } },
+			},
+		})
 
-        vim.keymap.set("n", "[t", function()
-            todo_comments.jump_prev()
-        end, { desc = "Previous todo comment" })
+		-- keymaps
+		vim.keymap.set("n", "]t", function()
+			todo_comments.jump_next()
+		end, { desc = "Next todo comment" })
 
-        todo_comments.setup()
-    end,
+		vim.keymap.set("n", "[t", function()
+			todo_comments.jump_prev()
+		end, { desc = "Previous todo comment" })
+	end,
 }
