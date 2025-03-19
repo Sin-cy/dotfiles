77b2259b2c81bb04732acfe267f4f6aafc3ac003
Lord Sinquaad <136789131+Sin-cy@users.noreply.github.com>
2025-03-19 14:57:00 +0700

    ...

--- nvim/.config/nvim/lua/sethy/plugins/gitstuff.lua
+++ nvim/.config/nvim/lua/sethy/plugins/gitstuff.lua
@@ -18,17 +18,17 @@ return {
 
                     local bufnr = vim.api.nvim_get_current_buf()
                     local opts = {buffer = bufnr, remap = false}
+
                     vim.keymap.set("n", "<leader>P", function()
                         vim.cmd.Git('push')
                     end, opts)
 
-                    -- rebase always
+                    -- NOTE: rebase always
                     vim.keymap.set("n", "<leader>p", function()
                         vim.cmd.Git({'pull',  '--rebase'})
                     end, opts)
 
-                    -- NOTE: It allows me to easily set the branch i am pushing and any tracking
-                    -- needed if i did not set the branch up correctly
+                    -- NOTE: easy set up branch that wasn't setup properly
                     vim.keymap.set("n", "<leader>t", ":Git push -u origin ", opts);
                 end,
             })
