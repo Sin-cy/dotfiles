784070930376ca7da9c3875268c0609dfc1112b6
Lord Sinquaad <136789131+Sin-cy@users.noreply.github.com>
2025-04-25 18:35:37 +0700

    ...

--- nvim/.config/nvim/lua/sethy/plugins/incline.lua
+++ nvim/.config/nvim/lua/sethy/plugins/incline.lua
@@ -2,33 +2,33 @@ return {
     -- Adding a filename to the Top Right
     {
         "b0o/incline.nvim",
-        dependencies = {"nvim-web-devicons"},
+        enabled = true,
+        dependencies = { "nvim-tree/nvim-web-devicons" },
         config = function()
             local devicons = require("nvim-web-devicons")
+
             require("incline").setup({
-                window = {
-                    padding = 0,
-                    margin = { horizontal = 0 },
+                hide = {
+                    only_win = false,
                 },
                 render = function(props)
-                    local filename = vim.fn.fnamemodify(vim.api.nvim_buf_get_name(props.buf), ":t")
-                    if filename == '' then
-                        filename = '[No Name]'
-                    end
-                    local icon, color = devicons.get_icon_color_by_filetype(filename)
+                    local bufname = vim.api.nvim_buf_get_name(props.buf)
+                    local filename = vim.fn.fnamemodify(bufname, ":t")
+                    if filename == '' then filename = '[No Name]' end
+
+                    local ext = vim.fn.fnamemodify(bufname, ":e")
+                    local icon, icon_color = devicons.get_icon(filename, ext, { default = true })
+
                     local modified = vim.bo[props.buf].modified
 
-                    local buffer = {
-                        icon and { " ", icon, " ", guifg = color }
-                        or "",
+                    return {
+                        { " ", icon, " ", guifg = icon_color },
+                        { filename, gui = modified and "bold" or "none" },
+                        modified and { " [+]", guifg = "#ff9e64" } or "",
                         " ",
-                        { filename, gui = modified and "bold" },
-                        " ",
-                        guibg = "#000",
                     }
-                    return buffer
                 end,
             })
         end,
-    },
+    }
 }
