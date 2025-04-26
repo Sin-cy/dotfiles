95d890a6c97144f58495a6c8c18acb838422abfd
Lord Sinquaad <136789131+Sin-cy@users.noreply.github.com>
2025-04-26 20:05:09 +0700

    updates...

--- nvim/.config/nvim/lua/sethy/plugins/tailwind-tools.lua
+++ nvim/.config/nvim/lua/sethy/plugins/tailwind-tools.lua
@@ -18,6 +18,13 @@ return {
             tailwindcolorizer.setup({
                 color_square_width = 2,
             })
+
+            vim.api.nvim_create_autocmd({ "BufReadPost", "BufNewFile" }, {
+                callback = function()
+                    vim.cmd("ColorizerAttachToBuffer")
+                end,
+            })
+
         end,
     },
 }
