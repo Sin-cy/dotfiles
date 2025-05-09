9135baba2913589f90a5859a47ce571d8f55dec5
Lord Sinquaad <136789131+Sin-cy@users.noreply.github.com>
2025-05-09 23:25:19 +0700

    changed branch ver. to master
    fix deprecated jump to definitino functions coming in nvim 0.12 & 13

--- nvim/.config/nvim/lua/sethy/plugins/telescope.lua
+++ nvim/.config/nvim/lua/sethy/plugins/telescope.lua
@@ -1,6 +1,7 @@
 return {
 	"nvim-telescope/telescope.nvim",
-	branch = "0.1.x",
+	branch = "master", -- using master to fix issues with deprecated to definition warnings 
+    -- '0.1.x' for stable ver.
 	dependencies = {
 		"nvim-lua/plenary.nvim",
 		{ "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
