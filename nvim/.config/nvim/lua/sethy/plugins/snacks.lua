3b21d49fd687b7569dcf6903344eda0984c803ec
Lord Sinquaad <136789131+Sin-cy@users.noreply.github.com>
2025-08-05 22:58:53 +0700

    fixed: snacks picker vim config path <leader>pc

--- nvim/.config/nvim/lua/sethy/plugins/snacks.lua
+++ nvim/.config/nvim/lua/sethy/plugins/snacks.lua
@@ -145,7 +145,7 @@ return {
 
             -- Snacks Picker
             { "<leader>pf", function() require("snacks").picker.files() end, desc = "Find Files (Snacks Picker)" },
-            { "<leader>pc", function() require("snacks").picker.files({ cwd = vim.fn.stdpath("config") }) end, desc = "Find Config File" },
+            { "<leader>pc", function() require("snacks").picker.files({ cwd = "~/dotfiles/nvim/.config/nvim/lua" }) end, desc = "Find Config File" },
             { "<leader>ps", function() require("snacks").picker.grep() end, desc = "Grep word" },
             { "<leader>pws", function() require("snacks").picker.grep_word() end, desc = "Search Visual selection or Word", mode = { "n", "x" } },
             { "<leader>pk", function() require("snacks").picker.keymaps({ layout = "ivy" }) end, desc = "Search Keymaps (Snacks Picker)" },
