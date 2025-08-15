45d21e295eacf1753c83f2b83af8e302a1dbd5af
Lord Sinquaad <136789131+Sin-cy@users.noreply.github.com>
2025-08-15 16:02:33 +0700

    fixed: cleanup old options that are now default in 0.11

--- nvim/.config/nvim/lua/sethy/core/options.lua
+++ nvim/.config/nvim/lua/sethy/core/options.lua
@@ -1,60 +1,48 @@
 -- vim.g.loaded_netrw = 0
 -- vim.g.loaded_netrwPlugin = 0
 -- vim.cmd("let g:netrw_liststyle = 3")
-vim.cmd("let g:netrw_banner = 0 ")
+-- Disable netrw banner
+vim.cmd("let g:netrw_banner = 0")
 
-vim.opt.guicursor = ""
+-- line numbers
 vim.opt.nu = true
 vim.opt.relativenumber = true
 
+-- indentation
 vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
-vim.opt.autoindent = true
 vim.opt.smartindent = true
 vim.opt.wrap = false
 
+-- backup and undo
 vim.opt.swapfile = false
 vim.opt.backup = false
 vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
 vim.opt.undofile = true
 
-vim.opt.incsearch = true
+-- search
 vim.opt.inccommand = "split"
 
-vim.opt.ignorecase = true
-vim.opt.smartcase = true
-
-vim.opt.termguicolors = true
+-- UI
 vim.opt.background = "dark"
-
 vim.opt.scrolloff = 8
 vim.opt.signcolumn = "yes"
 
--- Enable folding ( setup in nvim-ufo )
-vim.o.foldenable = true     -- Enable folding by default
-vim.o.foldmethod = "manual" -- Default fold method (change as needed)
-vim.o.foldlevel = 99        -- Open most folds by default
+-- folding (for nvim-ufo)
+vim.o.foldenable = true
+vim.o.foldmethod = "manual"
+vim.o.foldlevel = 99
 vim.o.foldcolumn = "0"
 
--- backspace
-vim.opt.backspace = { "start", "eol", "indent" }
+-- window splits
+vim.opt.splitright = true
+vim.opt.splitbelow = true
 
---split windows
-vim.opt.splitright = true --split vertical window to the right
-vim.opt.splitbelow = true --split horizontal window to the bottom
-
+-- misc
 vim.opt.isfname:append("@-@")
 vim.opt.updatetime = 50
 vim.opt.colorcolumn = "80"
-
--- clipboard
-vim.opt.clipboard:append("unnamedplus") --use system clipboard as default
-vim.opt.hlsearch = true
-
--- for easy mouse resizing, just incase
+vim.opt.clipboard:append("unnamedplus")
 vim.opt.mouse = "a"
-
--- gets rid of line with white spaces
-vim.g.editorconfig = true
