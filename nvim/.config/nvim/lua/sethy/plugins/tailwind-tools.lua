877ff6a40832910836b3e5e4e2f89e263d6edfcb
Lord Sinquaad <136789131+Sin-cy@users.noreply.github.com>
2026-01-12 17:07:17 +0700

    fixed colorizer rendering in markdown files

--- nvim/.config/nvim/lua/sethy/plugins/tailwind-tools.lua
+++ nvim/.config/nvim/lua/sethy/plugins/tailwind-tools.lua
@@ -12,17 +12,11 @@ return {
                 user_default_options = {
                     tailwind = true,
                 },
-                filetypes = { "html", "css", "javascript", "typescript", "jsx", "tsx", "vue", "svelte", "astro"},
+                filetypes = { "html", "css", "javascript", "typescript", "javascriptreact", "typescriptreact", "vue", "svelte", "astro"},
             })
 
             tailwindcolorizer.setup({
                 color_square_width = 2,
-            })
-
-            vim.api.nvim_create_autocmd({ "BufReadPost", "BufNewFile" }, {
-                callback = function()
-                    vim.cmd("ColorizerAttachToBuffer")
-                end,
             })
 
         end,
