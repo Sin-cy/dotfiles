b6727b1124520a7858bbe72e3cd5bf79cfed8f1e
Lord Sinquaad <136789131+Sin-cy@users.noreply.github.com>
2026-04-01 00:08:17 +0700

    feat: qol fixes

--- nvim/.config/nvim/lua/sethy/plugins/init.lua
+++ nvim/.config/nvim/lua/sethy/plugins/init.lua
@@ -1,17 +1,18 @@
 return {
-	"nvim-lua/plenary.nvim", --lua functions that many plugins use
-	"christoomey/vim-tmux-navigator", -- tmux & split window nav
+    "nvim-lua/plenary.nvim", -- functions multiple plugins need
+    "christoomey/vim-tmux-navigator", -- tmux & split window nav
     -- fixes the well know nvim bug
     {
         "folke/lazydev.nvim",
-        ft = "lua",
+        lazy = false,
         opts = {
             library = {
                 {
                     path = "${3rd}/plenary.nvim/lua",
                     words = { "plenary" }
                 },
+                { path = "LazyVim" },
             },
         },
     },
-  }
+}
