e2c16e035097216ee45a888105c6991e87c765dc
Lord Sinquaad <136789131+Sin-cy@users.noreply.github.com>
2026-04-03 23:37:35 +0700

    cleaned up options

--- nvim-nightly/.config/nvim-nightly/lua/sethy/core/init.lua
+++ nvim-nightly/.config/nvim-nightly/lua/sethy/core/init.lua
@@ -1,2 +1,7 @@
+-- vim.g.loaded_netrw = 0
+-- vim.g.loaded_netrwPlugin = 0
+-- vim.cmd("let g:netrw_liststyle = 3") Disable netrw banner
+vim.cmd("let g:netrw_banner = 0")
+
 require('sethy.core.options')
 require("sethy.core.keymaps")
--- nvim-nightly/.config/nvim-nightly/lua/sethy/core/options.lua
+++ nvim-nightly/.config/nvim-nightly/lua/sethy/core/options.lua
@@ -1,8 +1,3 @@
--- vim.g.loaded_netrw = 0
--- vim.g.loaded_netrwPlugin = 0
--- vim.cmd("let g:netrw_liststyle = 3") Disable netrw banner
-vim.cmd("let g:netrw_banner = 0")
-
 vim.opt.termguicolors = true
 -- line numbers 
 vim.opt.nu = true
