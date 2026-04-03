8a952effae61424212d315e97fea7cea7f86207b
Lord Sinquaad <136789131+Sin-cy@users.noreply.github.com>
2026-04-03 22:54:09 +0700

    feat: moved ui2

--- nvim-nightly/.config/nvim-nightly/init.lua
+++ nvim-nightly/.config/nvim-nightly/init.lua
@@ -1,3 +1,15 @@
+-- vim ui2
+require('vim._core.ui2').enable({
+    enable = true,
+    msg = {
+        target = "cmd", -- options: cmd(classic), msg(similar to noice)
+        pager = { height = 0.5 },
+        dialog = { height = 0.5 },
+        cmd    = { height = 0.5 },
+        msg   = { height = 0.5, timeout = 4500 },
+    },
+})
+
 require("sethy.core")
 require("sethy.pack")
 require("current-theme")
