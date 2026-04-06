7b5d056883b4c63b6c8fbb275f025cb185f27533
Lord Sinquaad <136789131+Sin-cy@users.noreply.github.com>
2026-04-06 16:24:49 +0700

    remove vim-pasta

--- nvim-nightly/.config/nvim-nightly/lua/sethy/plugins/vim-pasta.lua
+++
@@ -1,9 +0,0 @@
-return {
-    -- manual formatting for markdown
-   {
-      'sickill/vim-pasta',
-      config = function()
-         vim.g.pasta_enabled_filetypes = { 'markdown' } -- Enable for Markdown
-      end,
-   },
-}
