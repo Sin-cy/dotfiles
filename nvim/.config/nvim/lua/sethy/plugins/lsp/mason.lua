07cb1e2165815314624b36faab41630636df8834
Lord Sinquaad <136789131+Sin-cy@users.noreply.github.com>
2026-04-09 18:05:26 +0700

    feat: clean up mason + add biome

--- nvim/.config/nvim/lua/sethy/plugins/lsp/mason.lua
+++ nvim/.config/nvim/lua/sethy/plugins/lsp/mason.lua
@@ -1,24 +1,17 @@
 return {
-    "williamboman/mason.nvim",
+    "mason-org/mason.nvim",
     lazy = false,
     dependencies = {
-        "williamboman/mason-lspconfig.nvim",
+        "mason-org/mason-lspconfig.nvim",
         "WhoIsSethDaniel/mason-tool-installer.nvim",
         "hrsh7th/cmp-nvim-lsp",
         "neovim/nvim-lspconfig",
-        -- "saghen/blink.cmp",
     },
     config = function()
         -- import mason and mason_lspconfig
         local mason = require("mason")
         local mason_lspconfig = require("mason-lspconfig")
         local mason_tool_installer = require("mason-tool-installer")
-
-        -- NOTE: Moved these local imports below back to lspconfig.lua due to mason depracated handlers
-
-        -- local lspconfig = require("lspconfig")
-        -- local cmp_nvim_lsp = require("cmp_nvim_lsp")             -- import cmp-nvim-lsp plugin
-        -- local capabilities = cmp_nvim_lsp.default_capabilities() -- used to enable autocompletion (assign to every lsp server config)
 
         -- enable mason and configure icons
         mason.setup({
@@ -51,16 +44,14 @@ return {
 
         mason_tool_installer.setup({
             ensure_installed = {
-                "prettier", -- prettier formatter
-                "stylua",   -- lua formatter
-                "isort",    -- python formatter
+                "biome",
+                "prettier",
+                "stylua",
+                "isort",
                 "pylint",
                 "clangd",
                 "denols",
             },
-
-            -- NOTE: mason BREAKING Change! Removed setup_handlers
-            -- moved lsp configuration settings back into lspconfig.lua file
         })
     end,
 }
