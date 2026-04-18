145fb7458c738ce0371751592bd0440498a890f6
Lord Sinquaad <136789131+Sin-cy@users.noreply.github.com>
2026-04-18 18:58:45 +0700

    feat: updated comments

--- nvim-nightly/.config/nvim-nightly/lua/sethy/core/keymaps.lua
+++ nvim-nightly/.config/nvim-nightly/lua/sethy/core/keymaps.lua
@@ -24,13 +24,13 @@ vim.keymap.set("n", "N", "Nzzzv")
 vim.keymap.set("v", "<", "<gv", opts)
 vim.keymap.set("v", ">", ">gv", opts)
 
--- the how it be paste
+-- paste over selection without loosing yanked
 vim.keymap.set("x", "<leader>p", [["_dP]])
 
--- remember yanked
+-- better paste when in visual mode
 vim.keymap.set("v", "p", '"_dp', opts)
 
--- Copies or Yank to system clipboard
+-- yank to system clipboard
 vim.keymap.set("n", "<leader>Y", [["+Y]], opts)
 
 -- leader d delete wont remember as yanked/clipboard when delete pasting
@@ -39,6 +39,7 @@ vim.keymap.set({ "n", "v" }, "<leader>d", [["_d]])
 -- ctrl c as escape cuz Im lazy to reach up to the esc key
 vim.keymap.set("i", "<C-c>", "<Esc>")
 vim.keymap.set("n", "<C-c>", ":nohl<CR>", { desc = "Clear search hl", silent = true })
+
 -- format without prettier using the built in
 vim.keymap.set("n", "<leader>f", vim.lsp.buf.format)
 
