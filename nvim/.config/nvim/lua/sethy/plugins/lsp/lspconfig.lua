b6e877819afe6c7512ac69bbe7711819d22b82e1
Lord Sinquaad <136789131+Sin-cy@users.noreply.github.com>
2026-04-20 20:39:38 +0700

    updated keymap lsp restart

--- nvim/.config/nvim/lua/sethy/plugins/lsp/lspconfig.lua
+++ nvim/.config/nvim/lua/sethy/plugins/lsp/lspconfig.lua
@@ -46,7 +46,7 @@ return {
                 vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
 
                 opts.desc = "Restart LSP"
-                vim.keymap.set("n", "<leader>rs", ":LspRestart<CR>", opts)
+                vim.keymap.set("n", "<leader>rs", ":lsp restart<CR>", opts)
 
                 vim.keymap.set("i", "<C-h>", function()
                     vim.lsp.buf.signature_help()
