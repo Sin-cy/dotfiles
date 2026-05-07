ca13116138957d7a87a1f946fde36f5e9cf7a1cb
Lord Sinquaad <136789131+Sin-cy@users.noreply.github.com>
2026-05-08 05:35:06 +0700

    feat: clean up mini.lua

--- nvim-nightly/.config/nvim-nightly/lua/sethy/plugins/mini.lua
+++ nvim-nightly/.config/nvim-nightly/lua/sethy/plugins/mini.lua
@@ -32,19 +32,6 @@ require("mini.surround").setup({
         suffix_last = 'l', -- Suffix to search with "prev" method
         suffix_next = 'n', -- Suffix to search with "next" method
     },
-    -- Number of lines within which surrounding is searched
-    n_lines = 20,
-    -- Whether to respect selection type:
-    -- - Place surroundings on separate lines in linewise mode.
-    -- - Place surroundings on each line in blockwise mode.
-    respect_selection_type = false,
-    -- How to search for surrounding (first inside current line, then inside
-    -- neighborhood). One of 'cover', 'cover_or_next', 'cover_or_prev',
-    -- 'cover_or_nearest', 'next', 'prev', 'nearest'. For more details,
-    -- see `:h MiniSurround.config`.
-    search_method = 'cover',
-    -- Whether to disable showing non-error feedback
-    silent = false,
 })
 
 -- NOTE: mini.trailspace (get rid of whitespace)
